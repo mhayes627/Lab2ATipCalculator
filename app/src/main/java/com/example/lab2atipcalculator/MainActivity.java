ce99e759772901691dfa2998ba3cde26f0bf5366
Matthew Hayes <matthewhayes002@gmail.com>
2024-02-05 15:49:34 -0600

    Completed tip calculator

--- app/src/main/java/com/example/lab2atipcalculator/MainActivity.java
+++ app/src/main/java/com/example/lab2atipcalculator/MainActivity.java
@@ -3,13 +3,22 @@ package com.example.lab2atipcalculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextUtils;
 import android.view.View;
 
 import com.example.lab2atipcalculator.databinding.ActivityMainBinding;
 
+import java.text.Format;
+import java.text.NumberFormat;
+
 public class MainActivity extends AppCompatActivity {
 
     ActivityMainBinding binding;
+    Editable bill_input, percent_input, people_input;
+    int percent, people;
+    double bill, total;
+    String totalFormat;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,5 +26,45 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         View view = binding.getRoot();
         setContentView(view);
+
+        binding.calButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                bill_input = binding.billInput.getText();
+                percent_input =  binding.percentInput.getText();
+                people_input = binding.peopleInput.getText();
+
+                if (isValidCheck()){
+                    total = calculateTotal();
+                    totalFormat = NumberFormat.getCurrencyInstance().format(total);
+
+                    binding.totalText.setText(getResources().getString(R.string.total_text, totalFormat));
+                }
+            }
+        });
+    }
+
+    public boolean isValidCheck(){
+
+        bill = Double.parseDouble(bill_input.toString());
+        percent = Integer.parseInt(percent_input.toString());
+        people = Integer.parseInt(people_input.toString());
+
+        if (TextUtils.isEmpty(bill_input) || TextUtils.isEmpty(percent_input)
+            || TextUtils.isEmpty(people_input)){
+            return false;
+        }
+        else if (bill == 0 || percent == 0 || people ==0){
+            return false;
+        }
+        else{
+            return true;
+        }
+    }
+
+    public double calculateTotal(){
+        total = ((bill * percent/100) + bill)/ people;
+
+        return total;
     }
 }
